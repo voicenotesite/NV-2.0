befa7671e59cb9a3283aa0a59c7b11cc163baee0
voicenote <79220939+voicenotesite@users.noreply.github.com>
2026-03-24 17:03:33 +0100

    Test logiki

--- NV-Logic/Program.cs
+++ NV-Logic/Program.cs
@@ -1,7 +1,49 @@
 using System;
+using System.Collections.Generic;
 
-class Program {
-    static void Main() {
-        Console.WriteLine("--- NV-2.0: LOGIKA URUCHOMIONA ---");
+class Program
+{
+    // Definicja 4 głównych potęg świata NV-2.0
+    static string[] Factions = { "Neon Order", "Rust Marauders", "Silicon Cult", "Void Seekers" };
+    static List<string> ActiveNPCs = new List<string>();
+
+    static void Main()
+    {
+        Console.Title = "NV-2.0 | ENGINE LOGIC";
+        Console.WriteLine("--- NV-2.0: SYSTEM FRAKCJI AKTYWNY ---");
+        
+        Random rng = new Random();
+
+        // Symulujemy rozwój świata przez pierwsze 60 dni
+        for (int day = 1; day <= 60; day++) 
+        {
+            // Mechanika Bartosza: Nowy bot pojawia się co 10 dni
+            if (day % 10 == 0)
+            {
+                int botNumber = day / 10;
+                string assignedFaction;
+
+                if (botNumber <= 4)
+                {
+                    // Pierwsza czwórka to LIDERZY (każdy dostaje swoją gildię)
+                    assignedFaction = Factions[botNumber - 1];
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"[DZIEŃ {day}] POWSTAŁ LIDER: Bot #{botNumber} zakłada {assignedFaction}!");
+                }
+                else
+                {
+                    // Każdy kolejny to REKRUT - Totalny Random dołącza do jednej z gildii
+                    assignedFaction = Factions[rng.Next(0, 4)];
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"[DZIEŃ {day}] REKRUT: Bot #{botNumber} dołącza do {assignedFaction} (Total Random)");
+                }
+                
+                ActiveNPCs.Add($"Bot_{botNumber}@{assignedFaction}");
+                Console.ResetColor();
+            }
+        }
+
+        Console.WriteLine("\n--- AKTUALNY STAN ŚWIATA NV-2.0 ---");
+        foreach(var npc in ActiveNPCs) Console.WriteLine(npc);
     }
 }
